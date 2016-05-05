18e7a0e3135e27d95effc29a60c38a5cdd85691d
TomHoffman <t.hoffman@me.com>
2016-05-05 23:29:22 +0100

    amended script

---
+++ explanations.js
@@ -0,0 +1,13 @@
+//Explanations
+var favColor = 'blue';
+var myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Tom',
+	lastName: 'Hoffman',
+	favColors: ['blue', 'green', 'red'],
+	yearsAlive: 35,
+	isMale: true 
+};
+
---
+++ functions.js
@@ -0,0 +1,15 @@
+//Explanations
+
+var whatIsMyFavColor = function () {
+	return 'blue';
+}
+
+
+var doSomething = function () {
+	console.log("do Something please");
+}
+
+
+var area = function (width,height){
+	return width * height;
+}
